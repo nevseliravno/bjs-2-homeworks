3134d175665b75521dd3fea6f6d2621dd5f89380
sid <sid-ellada@mail.ru>
2025-01-15 19:07:07 +0300

    Домашнее задание к лекции 4 - Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+let student1 = new Student('Vika', 'female', 19);
+let student2 = new Student('Anton', 'male', 20);
+let student3 = new Student('Oleg', 'male', 17);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (this.marks) {
+		this.marks.push(...marksToAdd);
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) {
+		return 0;
+	}
+	let sum = this.marks.reduce((acc, item) => acc + item, 0);
+	return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
